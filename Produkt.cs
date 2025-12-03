bfe4077cad68280e28e86df4448499942117486e
Hryshko <valeriia.hryshko@training.lug-ag.de>
2025-12-03 18:16:07 +0100

    Erster Commit - funktionierende Version mit JSON und Produkt.cs

---
+++ Produkt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Shop
+{
+    internal class Produkt
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public decimal Preis { get; set; }
+        public bool Sichtbar { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using Mini_Shop;
+
+namespace Mini_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Legen Sie die UTF-8-Codierung fest, damit das Euro-Zeichen € korrekt angezeigt wird
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                // JSON aus der Datei lesen 
+                string json = File.ReadAllText("produkts.json");
+
+                // JSON in ein Wörterbuch deserialisieren, wobei der Schlüssel die Kategorie und der Wert die Produktliste ist
+                var kategorien = JsonSerializer.Deserialize<Dictionary<string, List<Produkt>>>(json);
+
+                // Produkte in Konsole ausgeben
+                Console.WriteLine("Liste der Produkte:\n");
+
+                foreach (var kategorie in kategorien)
+                {
+                    Console.WriteLine($"Kategorie: {kategorie.Key}");
+                    Console.WriteLine("{0,-5} | {1,-20} | {2,10}", "ID", "Name", "Preis (€)");
+                    Console.WriteLine(new string('-', 40));
+
+                    foreach (var produkt in kategorie.Value)
+                    {
+
+                        if (produkt.Sichtbar)
+                            Console.WriteLine("{0,-5} | {1,-20} | {2,10:F2}", produkt.ID, produkt.Name, produkt.Preis);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fechler: {ex.Message}");
+            }
+        }
+    }
+}
